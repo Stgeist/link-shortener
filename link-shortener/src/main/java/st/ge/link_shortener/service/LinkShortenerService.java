82a7073fb89c7f862895ca020ab4e349b6aa8098
Stefan Geist <stefangeist06@gmail.com>
2026-03-17 00:52:28 +0100

    feat: implement generateShortCode method

--- link-shortener/src/main/java/st/ge/link_shortener/service/LinkShortenerService.java
+++ link-shortener/src/main/java/st/ge/link_shortener/service/LinkShortenerService.java
@@ -1,13 +1,40 @@
 package st.ge.link_shortener.service;
 
+import java.security.SecureRandom;
+import java.util.HashMap;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class LinkShortenerService {
-    
-    public String shortenLink(String baseUrlString) {
-       String shortenedUrl = "Hello";
 
-       return shortenedUrl;
+    private HashMap<String, String> urlStorage = new HashMap<>();
+    
+    private static final String chars =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    private static final int shortCodeLength = 6;
+
+    private static final SecureRandom random = new SecureRandom();
+    
+    public String generateShortCode() {
+        
+        StringBuilder builder = new StringBuilder();
+
+        for(int i = 0; i >= shortCodeLength; i++) {
+            builder.append(chars.charAt(random.nextInt(chars.length())));
+        }
+
+        return builder.toString();
+    }
+
+    public String shortenUrl(String baseUrl) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'shortenUrl'");
+    }
+
+    public String getOriginalUrl(String shortCode) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getOriginalUrl'");
     }
 }
